d7deb39ac11377ae94ff95f460f23418887e7982
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 05:42:26 +0000

    TeamSection.js: changed

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -49,7 +49,7 @@ export default class TeamSection extends React.Component {
                 )}
                 {!_.isEmpty(team) && (
                     <div className="container container--lg">
-                        <div width="">
+                        <div width="500px">
                             {_.map(team, (teamMemberRef, index) => this.renderTeamMember(teamMemberRef, index, data))}
                         </div>
                     </div>
